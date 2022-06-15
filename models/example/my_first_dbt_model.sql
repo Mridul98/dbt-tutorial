772edaa1e843eeaddfd42f1de6deb9a85f64bd22
mridul <munmridul@gmail.com>
2022-06-15 16:31:18 +0600

    added first_dbt_model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,7 +7,12 @@
     Try changing "table" to "view" below
 */
 
--- {{ config(materialized='table') }}
+{{ 
+    config(
+        materialized='table',
+        schema="marketing"
+    ) 
+}}
 
 with source_data as (
 
